a488917849925405ab63f2af96d9854bbfc9b4c7
cmsbuild <>
2009-03-04 18:39:34 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- RPCMonitorClient-V03-03-02_pre4'.
    Sprout from master 2009-03-04 18:39:33 UTC David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch> '*** empty log message ***'
    Cherrypick from master 2008-12-15 16:28:31 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'modifications for 30x':
        BuildFile
    Delete:
        doc/html/index.html
        doc/html/overview.html
        python/RPCDaqInfo_cfi.py
        python/RPCEventSummary_cfi.py
        python/RPCFEDIntegrity_cfi.py
        python/RPCMon_SS_Dbx_Global_cfi.py
        python/RPCMonitorRaw_cfi.py
        python/RPCTier0Client_cff.py
        python/RPCTier0Source_cff.py
        test/.MuonDQMClient.xml
        test/.profile.xml
        test/.startMonitorClient
        test/QualityTests.xml
        test/RPCQualityTests.xml
        test/WebLib.js
        test/rpcdataintegrity_cfg.py
        test/sendCmdToApp.pl
        test/setup.sh
        test/style.css

--- python/RPCMonitorRaw_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-rpcRawDataCount = cms.EDAnalyzer("RPCMonitorRaw",
-  writeHistograms = cms.untracked.bool(False),
-  histoFileName = cms.untracked.string('rpcMonitorRaw.root')
-)
--- python/RPCTier0Client_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-# report summary
-from DQM.RPCMonitorClient.RPCEventSummary_cfi import *
-rpcEventSummary.Tier0 = False
-
-
-rpcTier0Client = cms.Sequence(rpcEventSummary)
