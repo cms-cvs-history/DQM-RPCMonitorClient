74ba0255d39980d93117515468fbd62902fe1458
cmsbuild <>
2009-11-24 14:06:54 +0000

    This commit was manufactured by cvs2git to create branch 'DQM- RPCMonitorClient-unlabeled-1.3.2'.
    Sprout from master 2009-11-24 14:06:52 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> '*** empty log message ***'
    Delete:
        BuildFile
        doc/html/index.html
        doc/html/overview.html
        interface/RPCBxTest.h
        interface/RPCChamberQuality.h
        interface/RPCClient.h
        interface/RPCClusterSizeTest.h
        interface/RPCDCSSummary.h
        interface/RPCDaqInfo.h
        interface/RPCDataCertification.h
        interface/RPCDeadChannelTest.h
        interface/RPCDqmClient.h
        interface/RPCEfficiencySecond.h
        interface/RPCEventSummary.h
        interface/RPCFEDIntegrity.h
        interface/RPCLinkSynchroHistoMaker.h
        interface/RPCMon_SS_Dbx_Global.h
        interface/RPCMonitorLinkSynchro.h
        interface/RPCMonitorRaw.h
        interface/RPCMultiplicityTest.h
        interface/RPCNoisyStripTest.h
        interface/RPCOccupancyTest.h
        interface/RPCQualityTests.h
        interface/RPCRawDataCountsHistoMaker.h
        interface/ReadMeFromFile.h
        interface/clientTools.h
        python/RPCChamberQuality_cfi.py
        python/RPCDCSSummary_cfi.py
        python/RPCDaqInfo_cfi.py
        python/RPCDataCertification_cfi.py
        python/RPCDqmClient_cfi.py
        python/RPCEfficiencySecondStep_cfi.py
        python/RPCEventSummary_cfi.py
        python/RPCFEDIntegrity_cfi.py
        python/RPCMon_SS_Dbx_Global_cfi.py
        python/RPCMonitorLinkSynchro_cfi.py
        python/RPCMonitorRaw_cfi.py
        python/RPCTier0Client_cff.py
        python/RPCTier0Source_cff.py
        python/RPC_Client_on_RootFile.py
        src/RPCBxTest.cc
        src/RPCChamberQuality.cc
        src/RPCClusterSizeTest.cc
        src/RPCDCSSummary.cc
        src/RPCDaqInfo.cc
        src/RPCDataCertification.cc
        src/RPCDeadChannelTest.cc
        src/RPCDqmClient.cc
        src/RPCEfficiencyBookSecondStep.cc
        src/RPCEfficiencySecond.cc
        src/RPCEventSummary.cc
        src/RPCFEDIntegrity.cc
        src/RPCLinkSynchroHistoMaker.cc
        src/RPCMon_SS_Dbx_Global.cc
        src/RPCMonitorLinkSynchro.cc
        src/RPCMonitorRaw.cc
        src/RPCMultiplicityTest.cc
        src/RPCNoisyStripTest.cc
        src/RPCOccupancyTest.cc
        src/RPCRawDataCountsHistoMaker.cc
        src/SealModule.cc
        test/.MuonDQMClient.xml
        test/.profile.xml
        test/.startMonitorClient
        test/QualityTests.xml
        test/RPCQualityTests.xml
        test/WebLib.js
        test/rpcBXStudies.py
        test/rpcClient_cfg.py
        test/rpcTier0Source_cfg.py
        test/rpcdataintegrity_cfg.py
        test/sendCmdToApp.pl
        test/setup.sh
        test/style.css

--- python/RPCMonitorRaw_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-rpcMonitorRaw = cms.EDAnalyzer("RPCMonitorRaw",
-  writeHistograms = cms.untracked.bool(False),
-  histoFileName = cms.untracked.string('rpcMonitorRaw.root')
-)
