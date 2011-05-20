1ef515d5a6bfb7f5556527201e31a03742c5a7be
cmsbuild <>
2011-05-20 12:40:03 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- RPCMonitorClient-V05-00-03-01'.
    Sprout from master 2011-03-02 21:46:02 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'bug fix'
    Cherrypick from master 2011-05-20 12:39:46 UTC Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch> 'bugfix':
        src/RPCChamberQuality.cc
        src/RPCDqmClient.cc
        src/RPCEventSummary.cc
        src/RPCOccupancyTest.cc

--- src/RPCDqmClient.cc
+++ src/RPCDqmClient.cc
@@ -96,7 +96,7 @@ void  RPCDqmClient::endRun(const edm::Run& r, const edm::EventSetup& c){
   if(offlineDQM_) this->getMonitorElements(r, c);
 
   float   rpcevents = minimumEvents_;
-  if(RPCEvents_) rpcevents = RPCEvents_ -> getIntValue();
+  if(RPCEvents_) rpcevents = RPCEvents_->getEntries();
   
   if(rpcevents < minimumEvents_) return;
   
@@ -188,20 +188,28 @@ void RPCDqmClient::endLuminosityBlock(edm::LuminosityBlock const& lumiSeg, edm::
     (*it)->endLuminosityBlock( lumiSeg, c);
   
   float   rpcevents = minimumEvents_;
-  if(RPCEvents_) rpcevents = RPCEvents_ -> getIntValue();
+  if(RPCEvents_) rpcevents = RPCEvents_->getEntries();
   
   if( rpcevents < minimumEvents_) return;
 
-  if( init_ &&  (lumiCounter_%prescaleGlobalFactor_ != 0 )) return;
+  if( !init_ ){
 
-  lumiCounter_ ++;
+    for (std::vector<RPCClient*>::iterator it = clientModules_.begin(); it!=clientModules_.end(); it++ ){
+      (*it)->clientOperation(c);
+    }
+    init_ = true;
+    return;
+  }
+
+  lumiCounter_++;
+
+  if (lumiCounter_%prescaleGlobalFactor_ != 0) return;
+
 
   for (std::vector<RPCClient*>::iterator it = clientModules_.begin(); it!=clientModules_.end(); it++ ){
     (*it)->clientOperation(c);
   }
 
-
-  init_ = true;
 }
 
 
